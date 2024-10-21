e58dc613b3f7d72b4928d0908789a797ee2084b8
akmalhossain <akmalforu@gmail.com>
2024-10-21 21:33:22 +0600

    showing blog in a initial stage

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,16 +1,24 @@
 import { useEffect, useState } from "react";
+import SingleBlog from "../SingleBlog/SingleBlog";
 
 
 const Blog = () => {
-    const [blogs, setBlogs]=useState([]);
-    useEffect(()=>{
+    const [blogs, setBlogs] = useState([]);
+    useEffect(() => {
         fetch('blogs.json')
-        .then(res=>res.json())
-        .then(data=>setBlogs(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setBlogs(data))
+    }, [])
     return (
         <div className="w-2/3">
             <h2 className="text-4xl">Blog{blogs.length}</h2>
+            {
+                blogs.map(blog => <SingleBlog
+                    key={blog.id}
+                    blog={blog}>
+                </SingleBlog>)
+            }
+
         </div>
     );
 };
--- src/components/SingleBlog/SingleBlog.jsx
+++ src/components/SingleBlog/SingleBlog.jsx
@@ -2,10 +2,29 @@
 import PropTypes from 'prop-types';
 
 const SingleBlog = ({blog}) => {
+    const {title, cover, author,author_img, reading_time, posted_date, hashtags} = blog;
     console.log(blog)
     return (
         <div>
-            
+            <img src={cover} alt={`cover picture of the title ${title}`} />
+            <div className='flex justify-between'>
+                <div className='flex items-center gap-5'>
+                    <img className='w-24 h-24 rounded-full' src={author_img} alt="" />
+                    <div>
+                        <h3 className='text-2xl'>{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                   <span>{reading_time} read</span> 
+                </div>
+            </div>
+            <h2 className="text-4xl">{title}</h2>
+            <p>
+                {
+                    hashtags.map((hash, index) => <span key={index}><a href="">{hash}</a></span>)
+                }
+            </p>
         </div>
     );
 };
